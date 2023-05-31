fb76b8e748feb4be6c190e74b49fc69a6ff83a23
distanceOn <distanceOn@yandex.ru>
2023-05-31 17:34:04 +0300

    Create  useState inputs for login

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,6 +1,36 @@
+import { useEffect, useRef, useState } from "react";
 import s from "./Input.module.css";
 
 export default function Input(props) {
+  // login
+  const [login, setLogin] = useState("");
+
+  const loginRef = useRef();
+
+  const handleLogin = () => {
+    setLogin(loginRef.current.value);
+  };
+  useEffect(() => {
+    if (login !== "") {
+      console.log(login);
+    }
+  }, [login]);
+
+  // password
+
+  const [pass, setPass] = useState("");
+
+  const passRef = useRef();
+
+  const handlePassword = () => {
+    setPass(passRef.current.value);
+  };
+  useEffect(() => {
+    if (pass !== "") {
+      console.log(pass);
+    }
+  });
+
   return (
     <div className={s.container}>
       {props.placeholder === "Логин" ? (
@@ -8,12 +38,18 @@ export default function Input(props) {
           className={s.input}
           type="text"
           placeholder={props.placeholder}
+          ref={loginRef}
+          onChange={handleLogin}
+          value={login}
         />
       ) : (
         <input
           className={s.input}
-          type="text"
+          type="password"
           placeholder={props.placeholder}
+          ref={passRef}
+          onChange={handlePassword}
+          value={pass}
         />
       )}
     </div>
